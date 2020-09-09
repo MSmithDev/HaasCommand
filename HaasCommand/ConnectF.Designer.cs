5a831e1efc0299eff69ab8872e71258752103587
Matt Smith <mattdsmith228@gmail.com>
2020-09-09 12:23:53 -0400

    Added main control form

--- HaasCommand/ConnectF.Designer.cs
+++ HaasCommand/ConnectF.Designer.cs
@@ -124,7 +124,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lable_connection1);
             this.Name = "ConnectF";
-            this.Text = "Haas Command";
+            this.Text = "Haas Command Connection";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
